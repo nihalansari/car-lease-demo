359a38267cd72f7687b2dac6e5d06b91a5f8e768
Nihal Ansari <nihalansari@googlemail.com>
2016-12-12 23:42:26 +0530

    Update vehicles.go
    bypass get check2

--- Chaincode/vehicle_code/src/vehicle_code/vehicles.go
+++ Chaincode/vehicle_code/src/vehicle_code/vehicles.go
@@ -620,8 +620,10 @@ func (t *SimpleChaincode) get_package_details(stub shim.ChaincodeStubInterface, 
 	fmt.Printf("Nihal Copy of chaincode running!")
 	bytes, err := json.Marshal(v)
 	
-	return bytes, nil															if err != nil { return nil, errors.New("GET_VEHICLE_DETAILS: Invalid vehicle object") }
-																
+	if err != nil { return nil, errors.New("GET_VEHICLE_DETAILS: Invalid vehicle object") }
+	else {
+		return bytes, nil }
+	
 	//if 		v.Owner	== caller		{
 	//			
 	//				return bytes, nil		
